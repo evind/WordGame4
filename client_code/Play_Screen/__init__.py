19f9334ea32430ef478ea25a1ce6edcaa053f030
Evin Darling <hi@evin.io>
2021-02-23 19:31:51 +0000

    Edited Play_Screen

--- client_code/Play_Screen/__init__.py
+++ client_code/Play_Screen/__init__.py
@@ -12,12 +12,12 @@ class Play_Screen(Play_ScreenTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
-    rules = {'valid_input': True, 'invalid_letters': [],
+    self.item['rules'] = {'valid_input': True, 'invalid_letters': [],
           'invalid_words': [], 'small_words': [],
           'duplicate_words': [], 'source_word_check': False}
-    prev_words = []
-    source_word = anvil.server.call('pick_source_word')
-    self.word_label.text = source_word
+    self.item['prev_words'] = []
+    self.item['source_word'] = anvil.server.call('pick_source_word')
+    self.word_label.text = self.item['source_word']
     
     self.item['curr_time'] = time.time()
     print("curr_time = ", self.item['curr_time'])
@@ -32,6 +32,7 @@ class Play_Screen(Play_ScreenTemplate):
     self.item['end_time'] = time.time()
     self.item['total_time'] = round(self.item['end_time'] - self.item['curr_time'], 4)
     print(self.item['total_time'])
-
+    user_input_list = self.answer_box.text.split()
+    print(user_input_list)
 
 
