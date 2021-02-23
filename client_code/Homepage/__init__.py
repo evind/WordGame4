594cb22814103b8b893a6420560539e1cf879e63
Evin Darling <hi@evin.io>
2021-02-23 18:03:34 +0000

    Edited Homepage and Play_Screen

--- client_code/Homepage/__init__.py
+++ client_code/Homepage/__init__.py
@@ -13,17 +13,9 @@ class Homepage(HomepageTemplate):
 
   def play_button_click(self, **event_args):
     """This method is called when the button is clicked"""
-    rules = {'valid_input': True, 'invalid_letters': [],
-             'invalid_words': [], 'small_words': [],
-             'duplicate_words': [], 'source_word_check': False}
-    prev_words = []
+
     # leaderboard stuff
     # create dictionary
-    for i in range(0, 1000):
-      print(i)
-      source_word = anvil.server.call('pick_source_word')
-      if len(source_word) < 8:
-        print(source_word, " < 8")
     #source_word = anvil.server.call('pick_source_word')
     #print(source_word)
     #open_form('Play_Screen')
--- client_code/Play_Screen/__init__.py
+++ client_code/Play_Screen/__init__.py
@@ -10,4 +10,7 @@ class Play_Screen(Play_ScreenTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
-    
+    rules = {'valid_input': True, 'invalid_letters': [],
+          'invalid_words': [], 'small_words': [],
+          'duplicate_words': [], 'source_word_check': False}
+    prev_words = []
